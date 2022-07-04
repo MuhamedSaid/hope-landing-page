7da8f2e9aa5d1c56fd793c2f18a0a78c18941521
MuhamedSaid <muhammed.said1312@gmail.com>
2022-07-04 23:45:21 +0200

    link pages together

--- script.js
+++ script.js
@@ -104,19 +104,22 @@ const allLinks = document.querySelectorAll('a:link');
 
 allLinks.forEach(function (link) {
   link.addEventListener('click', function (e) {
-    const href = link.getAttribute('href');
+    if (!link.classList.contains('default-link')) {
+      e.preventDefault();
+      const href = link.getAttribute('href');
 
-    // Scroll back to top
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
+      // Scroll back to top
+      if (href === '#')
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
 
-    // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
+      // Scroll to other links
+      if (href !== '#' && href.startsWith('#')) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: 'smooth' });
+      }
     }
 
     // Close mobile navigation
